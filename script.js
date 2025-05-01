c8299c4426ed326183d9c72640351001db8d0411
bunyadovaytac <aytacbunyadova2006@gmail.com>
2025-05-01 17:54:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-// Add new skill
+// Skill əlavə et
 function addSkill() {
-  const skillInput = document.getElementById("new-skill");
-  const skillValue = skillInput.value.trim();
-  if (skillValue !== "") {
-    const li = document.createElement("li");
-    li.textContent = skillValue;
-    document.getElementById("skills-list").appendChild(li);
-    skillInput.value = "";
+    const skillInput = document.getElementById("new-skill");
+    const skillValue = skillInput.value.trim();
+    if (skillValue !== "") {
+      const li = document.createElement("li");
+      li.textContent = skillValue;
+      document.getElementById("skills-list").appendChild(li);
+      skillInput.value = "";
+    }
   }
-}
-
-// Add new education
-function addEducation() {
-  const eduInput = document.getElementById("new-edu");
-  const eduValue = eduInput.value.trim();
-  if (eduValue !== "") {
-    const li = document.createElement("li");
-    li.textContent = eduValue;
-    document.getElementById("education-list").appendChild(li);
-    eduInput.value = "";
+  
+  // Education əlavə et
+  function addEducation() {
+    const eduInput = document.getElementById("new-edu");
+    const eduValue = eduInput.value.trim();
+    if (eduValue !== "") {
+      const li = document.createElement("li");
+      li.textContent = eduValue;
+      document.getElementById("education-list").appendChild(li);
+      eduInput.value = "";
+    }
   }
-}
-
-// Add new work experience
-function addWork() {
-  const workInput = document.getElementById("new-work");
-  const workValue = workInput.value.trim();
-  if (workValue !== "") {
-    const li = document.createElement("li");
-    li.textContent = workValue;
-    document.getElementById("work-list").appendChild(li);
-    workInput.value = "";
+  
+  // Work təcrübəsi əlavə et
+  function addWork() {
+    const workInput = document.getElementById("new-work");
+    const workValue = workInput.value.trim();
+    if (workValue !== "") {
+      const li = document.createElement("li");
+      li.textContent = workValue;
+      document.getElementById("work-list").appendChild(li);
+      workInput.value = "";
+    }
   }
-}
+  
